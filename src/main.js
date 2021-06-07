a5e0dddfd08547c93709cedecf57c1a56a070afe
Qiurui Chen <q.chen@skimgroup.com>
2021-06-07 12:41:21 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/tailwind.css'
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue'
+import aws_exports from './aws-exports'
+
+// import the aws-export.js file
+Amplify.configure(aws_exports);
 
 Vue.config.productionTip = false
 
